7ee96dfe7c160b6711960f63c2c5cd0f5b09ced3
Oleg Martynov <fjolne.yngling@gmail.com>
2023-05-13 10:54:02 +0100

    always --impure + profile can be passed as 1st arg

--- switch.sh
+++ switch.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . ~/.nix-profile/etc/profile.d/nix.sh
-nix run home-manager/master -- switch -b bak --flake "$SCRIPT_DIR" "$@"
+nix run home-manager/master -- switch --impure -b bak --flake "$SCRIPT_DIR${1:+#$1}"
