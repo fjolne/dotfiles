9f4f80ce0afd8a216bd41d3321598b2b87e24c5e
Oleg Martynov <fjolne.yngling@gmail.com>
2023-05-13 10:41:54 +0100

    pass install.sh args to switch.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-"$SCRIPT_DIR"/switch.sh
+"$SCRIPT_DIR"/switch.sh "$@"
