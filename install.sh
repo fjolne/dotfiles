e7c1d21bac3d067fbf50c1caa5c9044ff15b5be9
Oleg Martynov <fjolne.yngling@gmail.com>
2023-05-16 12:19:53 +0100

    do not append to nix.conf on install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
-mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 "$SCRIPT_DIR"/switch.sh "$@"
