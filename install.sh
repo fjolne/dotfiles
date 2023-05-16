bc647a237da61c014c15036c2a55eb3edbd2e92e
Oleg Martynov <fjolne.yngling@gmail.com>
2023-05-16 12:26:17 +0100

    added git-crypt before ./switch.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+echo 'nix run nixpkgs#git-crypt -- "$@"' > ~/.local/bin/git-crypt && chmod u+x ~/.local/bin/git-crypt
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 "$SCRIPT_DIR"/switch.sh "$@"
