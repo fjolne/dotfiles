17f80d315eb280fb62148c9330b2c100660daff2
Oleg Martynov <fjolne.yngling@gmail.com>
2023-05-22 15:12:12 +0100

    explicit firewall rules

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -135,11 +135,9 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
