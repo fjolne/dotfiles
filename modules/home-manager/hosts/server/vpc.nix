adb14c35da8851d7b45d8006877dc82163045ebb
Oleg Martynov <fjolne.yngling@gmail.com>
2023-06-08 13:37:27 +0100

    locale magic

--- modules/home-manager/hosts/server/vpc.nix
+++ modules/home-manager/hosts/server/vpc.nix
@@ -5,6 +5,9 @@
 
   programs.bash = {
     profileExtra = ''
+      export LANG=en_US.utf-8
+      export LC_ALL=en_US.utf-8
+      export LC_CTYPE=en_US.utf-8
       export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
     '';
   };
