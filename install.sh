0c3d800ee81324c7bf0f75225146ba2ff48f3225
Oleg Martynov <fjolne.yngling@gmail.com>
2023-06-13 14:55:34 +0100

    gpg_tty fix?

--- install.sh
+++ install.sh
@@ -9,11 +9,10 @@ mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" > ~/
 
 if [[ "${CLONE:-}" == true ]]; then
     echo "Paste your private (passhprase-protected) GPG key:"
-    key=$(cat) nix shell nixpkgs#git nixpkgs#git-crypt nixpkgs#gnupg nixpkgs#pinentry-gtk2 <<"EOF"
+    key=$(cat) GPG_TTY=$(tty) nix shell nixpkgs#git nixpkgs#git-crypt nixpkgs#gnupg nixpkgs#pinentry-gtk2 <<"EOF"
     git clone https://github.com/fjolne/dotfiles && cd dotfiles
     install -d -m700 -o $USER -g $USER ~/.gnupg \
     && printf "grab\npinentry-program $(which pinentry)" > ~/.gnupg/gpg-agent.conf
-    export GPG_TTY=$(tty)
     gpg --import <<< "$key"
     git-crypt unlock
 EOF
