6a523a8601b7b13c84811bb3c6c5bc9443abf3a9
Oleg Martynov <fjolne.yngling@gmail.com>
2023-06-29 10:22:45 +0100

    tweaked ssh config for keep-alive

--- modules/home-manager/hosts/desktop/common.nix
+++ modules/home-manager/hosts/desktop/common.nix
@@ -4,7 +4,12 @@ with lib;
 {
   imports = [ ../base/common.nix ];
 
-  programs.ssh.extraConfig = utils.readSecretFile ./ssh_config;
+  # programs.ssh.extraConfig = utils.readSecretFile ./ssh_config;
+  programs.ssh = {
+    serverAliveInterval = 60;
+    serverAliveCountMax = 30;
+    extraConfig = utils.readSecretFile ./ssh_config;
+  };
 
   programs.keychain = {
     inheritType = "local-once";
