c10a6b2c7193f319291e48eff71e8a517eede1e1
Oleg Martynov <fjolne.yngling@gmail.com>
2023-06-30 10:14:40 +0100

    use regular vscode on g2

--- modules/home-manager/hosts/desktop/g2-nixos.nix
+++ modules/home-manager/hosts/desktop/g2-nixos.nix
@@ -4,16 +4,7 @@
   imports = [ ./nixos.nix ];
 
   home.packages = with pkgs; [
-    ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
-      src = (builtins.fetchTarball {
-        url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-        sha256 = "0vklckqw8hc74ggqr7snqidzf0h7366sdxq72rrvpl7kz1nh3kxs";
-      });
-      version = "latest";
-    }))
-    (pkgs.writeShellScriptBin "code" ''
-      code-insiders "$@"
-    '')
+    vscode
 
     pcsx2
     retroarchFull
