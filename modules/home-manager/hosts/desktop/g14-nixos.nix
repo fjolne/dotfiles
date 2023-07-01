ae440b0e731030108dac6b656f7cc5f6e759c9d4
Oleg Martynov <fjolne.yngling@gmail.com>
2023-07-01 15:34:20 +0100

    setup vscode via home-manager

--- modules/home-manager/hosts/desktop/g14-nixos.nix
+++ modules/home-manager/hosts/desktop/g14-nixos.nix
@@ -3,14 +3,23 @@
 {
   imports = [ ./nixos.nix ];
 
-  home.packages = with pkgs; [
-    ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
+
+  programs.vscode = {
+    enable = true;
+    package = ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs (oldAttrs: {
+      pname = "vscode-insiders";
       src = (builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
         sha256 = "1ddr9fjlp4sk176b96c4i2qv5wzksjr5m929k3czncr6jfqsf6c3";
       });
       version = "latest";
-    }))
+    }));
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+    ];
+  };
+
+  home.packages = with pkgs; [
     (pkgs.writeShellScriptBin "code" ''
       code-insiders "$@"
     '')
