b1f9e077afbb1ec559dc582c0e07b9f323d88202
Oleg Martynov <fjolne.yngling@gmail.com>
2023-07-13 14:00:38 +0100

    added ll/lla nu aliases

--- modules/home-manager/hosts/desktop/g14.nix
+++ modules/home-manager/hosts/desktop/g14.nix
@@ -8,6 +8,8 @@
     enable = true;
     extraConfig = ''
       let-env config = { show_banner: false }
+      def ll [] { ls -l | select uid mode name type size modified target | sort-by type name -i }
+      def lla [] { ls -la | select uid mode name type size modified target | sort-by type name -i }
     '';
   };
 
