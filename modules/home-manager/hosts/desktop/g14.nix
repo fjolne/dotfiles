bc3d09d75a8aed3471d94b282b8f04ea24ecef39
Oleg Martynov <fjolne.yngling@gmail.com>
2023-07-14 11:46:11 +0100

    nushell: import home.sessionVariables

--- modules/home-manager/hosts/desktop/g14.nix
+++ modules/home-manager/hosts/desktop/g14.nix
@@ -8,6 +8,16 @@
     enable = true;
     extraConfig = ''
       let-env config = { show_banner: false }
+
+      cat $"($env.HOME)/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      | lines
+      | where $it =~ ^export
+      | str replace "export " ""
+      | split column =
+      | update column2 { |it| ^bash -c $"echo -n ($it.column2)" | str join }
+      | transpose -r -d
+      | load-env
+
       def ll [] { ls -l | select uid mode name type size modified target | sort-by type name -i }
       def lla [] { ls -la | select uid mode name type size modified target | sort-by type name -i }
     '';
