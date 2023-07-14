6c27001ffba40e2aa90138e26cc1650783a78e6b
Oleg Martynov <fjolne.yngling@gmail.com>
2023-07-14 13:24:24 +0100

    flake: added pkgs.unstable overlay

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "nix system configurations";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
@@ -18,40 +18,41 @@
   outputs =
     { self
     , nixpkgs
+    , nixpkgs-unstable
     , flake-utils
     , nixos-hardware
     , home-manager
     , ...
     } @ inputs:
     let
-      mkNixosConfig =
-        { system ? "x86_64-linux"
-        , nixpkgs ? inputs.nixpkgs
-        , hardwareModules
-        , baseModules ? [
+      mkNixosConfig = { system, hardwareModules, extraModules ? [ ] }:
+        let
+          baseModules = [
             home-manager.nixosModules.home-manager
             ./modules/nixos
-          ]
-        , extraModules ? [ ]
-        }:
+          ];
+        in
         nixpkgs.lib.nixosSystem {
           inherit system;
           modules = baseModules ++ hardwareModules ++ extraModules;
           specialArgs = { inherit self inputs nixpkgs; };
         };
-      mkHomeConfig =
-        { username
-        , system ? "x86_64-linux"
-        , nixpkgs ? inputs.nixpkgs
-        , baseModules ? [ ]
-        , extraModules ? [ ]
-        }:
+      mkHomeConfig = { system, username, extraModules ? [ ] }:
         let
+          baseModules = [ ];
           lib = nixpkgs.lib.extend
             (final: prev: (import ./lib final) // home-manager.lib);
+          unstableOverlay = final: prev: {
+            unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
+          pkgs = (nixpkgs.legacyPackages.${system}.extend unstableOverlay)
+            // { config.allowUnfree = true; };
         in
-        inputs.home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system} // { config.allowUnfree = true; };
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
           extraSpecialArgs = { inherit self inputs nixpkgs lib username; };
           modules = baseModules ++ extraModules;
         };
