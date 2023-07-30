4dfe2ab3e79f32d8be4b9703b26e0bd093f9905e
Oleg Martynov <fjolne.yngling@gmail.com>
2023-07-30 11:19:58 +0100

    updated nushell config

--- modules/home-manager/hosts/desktop/g14.nix
+++ modules/home-manager/hosts/desktop/g14.nix
@@ -4,10 +4,26 @@
   imports = [ ./common.nix ];
 
   programs.bash.enable = lib.mkForce false;
+  programs.direnv.enableNushellIntegration = false;
   programs.nushell = {
     enable = true;
     extraConfig = ''
-      let-env config = { show_banner: false }
+      let-env config = {
+        show_banner: false,
+        menus: [],
+        keybindings: [],
+        hooks: {
+          env_change: {
+            PWD: [{
+              code: "
+                let direnv = (direnv exec / direnv export json | from json)
+                let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
+                $direnv | load-env
+              "
+            }]
+          }
+        }
+      }
 
       cat $"($env.HOME)/.nix-profile/etc/profile.d/hm-session-vars.sh"
       | lines
@@ -20,6 +36,12 @@
 
       def ll [] { ls -l | select uid mode name type size modified target | sort-by type name -i }
       def lla [] { ls -la | select uid mode name type size modified target | sort-by type name -i }
+
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/cargo/cargo-completions.nu *
+      use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/poetry/poetry-completions.nu *
     '';
   };
+  programs.zoxide.enable = true;
 }
