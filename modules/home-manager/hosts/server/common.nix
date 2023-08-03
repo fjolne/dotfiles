8ed723d675d120d02cf66036f1352e4d47ac968a
Oleg Martynov <fjolne.yngling@gmail.com>
2023-08-03 10:47:17 +0100

    removed rersync

--- modules/home-manager/hosts/server/common.nix
+++ modules/home-manager/hosts/server/common.nix
@@ -10,12 +10,4 @@ with lib;
     inheritType = "any-once";
     keys = [ "id_rsa" ];
   };
-
-  home.packages = [
-    (pkgs.writeShellScriptBin "rersync" ''
-      while [ $? -ne 0 ] ; do
-          timeout 240 rsync --partial "$@"
-      done
-    '')
-  ];
 }
