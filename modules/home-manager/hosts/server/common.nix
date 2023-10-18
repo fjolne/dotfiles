3e5df612cbb9bd5f3c48f58ca5e2d194b268b169
Oleg Martynov <fjolne.yngling@gmail.com>
2023-10-18 18:08:20 +0100

    removed ssh_config from server

--- modules/home-manager/hosts/server/common.nix
+++ modules/home-manager/hosts/server/common.nix
@@ -4,8 +4,6 @@ with lib;
 {
   imports = [ ../base ];
 
-  programs.ssh.extraConfig = utils.readSecretFile ./ssh_config;
-
   programs.keychain = {
     enable = true;
     inheritType = "local-once";
