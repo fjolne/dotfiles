4d2f92c7cf5ef9ae9a9dbd2f64191cbc1ecb08ef
Oleg Martynov <fjolne.yngling@gmail.com>
2024-02-26 20:28:11 +0000

    added fzf back

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -78,6 +78,13 @@ with lib;
     ];
   };
 
+  programs.fzf = {
+    enable = true;
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f";
+    changeDirWidgetCommand = "${pkgs.fd}/bin/fd --type d";
+    defaultOptions = [ "--border" "--inline-info" ];
+  };
+
   programs.bash = {
     enable = false;
 
