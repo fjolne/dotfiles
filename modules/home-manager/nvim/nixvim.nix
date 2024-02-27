a57de0582fd2b34b7072301ca64db583f689cd07
Oleg Martynov <fjolne.yngling@gmail.com>
2024-02-27 17:43:10 +0000

    updated nixvim

--- modules/home-manager/nvim/nixvim.nix
+++ modules/home-manager/nvim/nixvim.nix
@@ -8,34 +8,46 @@
     globals.maplocalleader = " ";
     options = {
       clipboard = "unnamedplus";
-      number = true; # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      shiftwidth = 2; # Tab width should be 2
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2; # tab width, but mostly managed by vim-sleuth
+      ignorecase = true;
+      smartcase = true;
+      updatetime = 250;
+      timeoutlen = 300;
+      inccommand = "split";
+      scrolloff = 999; # keep cursor centered
+      list = true;
+      listchars = { tab = "» "; trail = "·"; nbsp = "␣"; };
     };
 
     colorschemes.gruvbox.enable = true;
 
     keymaps = [
-      {
-        action = "<cmd>Telescope find_files<CR>";
-        key = "<C-p>";
-      }
-      {
-        action = "<cmd>Telescope live_grep<CR>";
-        key = "<C-f>";
-      }
-      {
-        action = "<cmd>Telescope buffers<CR>";
-        key = "<C-b>";
-      }
-      {
-        action = "<cmd>Telescope help_tags<CR>";
-        key = "<C-/>";
-      }
+      { mode = "n"; key = "<Esc>"; action = "<cmd>nohlsearch<CR>"; }
+      { mode = "n"; key = "<C-s>"; action = "<cmd>write<CR>"; }
+      { mode = "t"; key = "<Esc>"; action = "<C-\\><C-n>"; }
     ];
+
+    plugins.gitsigns.enable = true;
+    plugins.diffview.enable = true;
+    plugins.comment-nvim.enable = true;
+    plugins.auto-session.enable = true;
+
+    plugins.toggleterm = {
+      enable = true;
+      openMapping = "<C-`>";
+      size = 48;
+    };
 
     plugins.telescope = {
       enable = true;
+      keymaps = {
+        "<C-p>" = "find_files";
+        "<C-f>" = "live_grep";
+        "<C-b>" = "buffers";
+        "<C-/>" = "help_tags";
+      };
     };
 
     plugins.nvim-cmp = {
@@ -45,35 +57,41 @@
         { name = "nvim_lsp"; }
         { name = "path"; }
         { name = "buffer"; }
-        { name = "luasnip"; }
+        # { name = "luasnip"; }
       ];
       mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-e>" = "cmp.mapping.close()";
         "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = {
-          action = ''
-            function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              elseif check_backspace() then
-                fallback()
-              else
-                fallback()
-              end
-            end
-          '';
+        "<S-Tab>" = {
+          action = "cmp.mapping.select_prev_item()";
           modes = [ "i" "s" ];
         };
+        "<Tab>" = {
+          action = "cmp.mapping.select_next_item()";
+          modes = [ "i" "s" ];
+        };
+        # "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        # "<C-f>" = "cmp.mapping.scroll_docs(4)";
       };
     };
 
     plugins.lsp = {
       enable = true;
+      keymaps.lspBuf = {
+        gh = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+        "<leader>af" = "format";
+        "<leader>ar" = "rename";
+      };
       servers = {
-        nil_ls.enable = true;
+        nil_ls = {
+          enable = true;
+          settings.formatting.command = [ "nixpkgs-fmt" ];
+        };
         lua-ls = {
           enable = true;
           settings.telemetry.enable = false;
@@ -87,10 +105,7 @@
     };
 
     extraPlugins = with pkgs.vimPlugins; [
-      {
-        plugin = comment-nvim;
-        config = "lua require(\"Comment\").setup()";
-      }
+      vim-sleuth
     ];
   };
 }
