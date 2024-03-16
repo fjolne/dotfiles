95074d647bc151a93f9389400881e78f0b2ec343
Oleg Martynov <fjolne.yngling@gmail.com>
2024-03-16 17:05:59 +0000

    kitty: added more keys

--- modules/home-manager/desktop/kitty.nix
+++ modules/home-manager/desktop/kitty.nix
@@ -9,16 +9,18 @@
       enable_audio_bell = false;
       update_check_interval = 0;
       window_padding_width = 2;
-      enabled_layouts = "vertical";
+      enabled_layouts = "splits, stack";
     };
     keybindings = {
-      "ctrl+shift+enter" = "launch --type=tab --tab-title=current --location=after --cwd=current";
-      "ctrl+shift+b" = "launch --allow-remote-control kitty +kitten broadcast --match-tab state:focused";
-      "ctrl+shift+/" = "launch --type=window";
-      "ctrl+shift+alt+up" = "neighboring_window up";
-      "ctrl+shift+alt+down" = "neighboring_window down";
-      "ctrl+shift+alt+left" = "neighboring_window left";
-      "ctrl+shift+alt+right" = "neighboring_window right";
+      "kitty_mod+enter" = "launch --type=tab --tab-title=current --location=after --cwd=current";
+      "kitty_mod+b" = "launch --allow-remote-control kitty +kitten broadcast --match-tab state:focused";
+      "kitty_mod+/" = "launch --location=hsplit";
+      "kitty_mod+alt+/" = "launch --location=vsplit";
+      "kitty_mod+f" = "toggle_layout stack";
+      "kitty_mod+alt+up" = "neighboring_window up";
+      "kitty_mod+alt+down" = "neighboring_window down";
+      "kitty_mod+alt+left" = "neighboring_window left";
+      "kitty_mod+alt+right" = "neighboring_window right";
     };
     theme = "Gruvbox Dark";
     font = {
