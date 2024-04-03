ef68fba79a2d84ef069859a1d73129816cc4cf76
Oleg Martynov <fjolne.yngling@gmail.com>
2024-04-03 11:20:08 +0000

    configuration.nix cleanup

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -7,34 +7,41 @@
 with self.inputs;
 with lib;
 {
-  nix.package = nix.packages.${pkgs.hostPlatform.system}.nix;
-  nix.registry.nixpkgs.flake = self.inputs.nixpkgs;
-  nix.nixPath = ["nixpkgs=flake:nixpkgs"];
+  imports = [
+    nixos-hardware.nixosModules.common-gpu-nvidia-disable
+    ./nix-ld.nix
+  ];
 
-  # Bootloader.
+  # use latest `nix`
+  nix.package = nix.packages.${pkgs.hostPlatform.system}.nix;
+  # `nix run nixpkgs#hello` will use nixpkgs from this flake
+  nix.registry.nixpkgs.flake = self.inputs.nixpkgs;
+  # <nixpkgs> will resolve to this flake
+  nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.substituters = [
+    "https://cuda-maintainers.cachix.org"
+    "https://numtide.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+  ];
+  nixpkgs.config.allowUnfree = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Setup keyfile
   boot.initrd.secrets = {
     "/crypto_keyfile.bin" = null;
   };
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "UTC";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -47,119 +54,14 @@ with lib;
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
+  services.xserver.layout = "us";
+  services.xserver.xkbVariant = "";
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.gnome-keyring.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  programs.fish.enable = true;
-  users.users.fjolne = {
-    isNormalUser = true;
-    description = "Oleg Martynov";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    shell = pkgs.fish;
-    openssh.authorizedKeys = {
-      keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyva32b9/JANUGZEgQny/wemAETo4z6wAkF16CLk7fF"
-      ];
-    };
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment = {
-    variables = {
-      # QT_QPA_PLATFORMTHEME = "Adwaita-Dark";
-    };
-    shells = with pkgs; [ fish ];
-    systemPackages = with pkgs; [
-      git
-      gnumake
-      git-crypt
-      pinentry
-    ];
-  };
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.substituters = [
-    "https://cuda-maintainers.cachix.org"
-    "https://numtide.cachix.org"
-  ];
-  nix.settings.trusted-public-keys = [
-    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = false;
-    pinentryFlavor = "gtk2";
-  };
-  services.tailscale.enable = true;
-  services.tailscale.package = pkgs-unstable.tailscale;
-  services.tailscale.useRoutingFeatures = "client";
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ ];
-  networking.firewall.allowedUDPPorts = [ ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-  };
-
-  imports = [
-    nixos-hardware.nixosModules.common-gpu-nvidia-disable
-    ./nix-ld.nix
-  ];
   hardware.nvidia.prime.offload.enable = false;
   hardware.nvidia.powerManagement.enable = false;
   hardware.nvidia.dynamicBoost.enable = mkForce false;
@@ -173,12 +75,67 @@ with lib;
     };
   };
 
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #jack.enable = true;
+  };
+
+  # Don't forget to set a password with ‘passwd’.
+  programs.fish.enable = true;
+  users.users.fjolne = {
+    isNormalUser = true;
+    description = "Oleg Martynov";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.fish;
+    openssh.authorizedKeys = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyva32b9/JANUGZEgQny/wemAETo4z6wAkF16CLk7fF"
+      ];
+    };
+  };
+
+  environment = {
+    variables = {
+      # QT_QPA_PLATFORMTHEME = "Adwaita-Dark";
+    };
+    shells = with pkgs; [ fish ];
+    systemPackages = with pkgs; [
+      git
+      gnumake
+      git-crypt
+      pinentry
+    ];
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = false;
+    pinentryFlavor = "gtk2";
+  };
+  services.openssh.enable = false;
+  services.tailscale.enable = true;
+  services.tailscale.package = pkgs-unstable.tailscale;
+  services.tailscale.useRoutingFeatures = "client";
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedUDPPorts = [ ];
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
+
   virtualisation.docker.enable = true;
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "fjolne" ];
-
-
-  programs.steam.enable = true;
   services.flatpak.enable = true;
 
   hardware.bluetooth.settings = {
@@ -193,4 +150,6 @@ with lib;
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
+
+  system.stateVersion = "23.05"; # do not change
 }
