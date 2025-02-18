a43a0eefad21dab02e81a2c39b5e583d258fdf6d
Oleg Martynov <fjolne.yngling@gmail.com>
2025-02-18 08:00:08 +0000

    time Europe/Lisbon

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -33,7 +33,7 @@ with lib;
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   networking.networkmanager.enable = true;
 
-  time.timeZone = "Asia/Bangkok";
+  time.timeZone = "Europe/Lisbon";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
