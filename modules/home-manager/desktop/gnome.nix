fa5ef2771daf057de004b2bf8ed5b7e25a70b933
Oleg Martynov <fjolne.yngling@gmail.com>
2026-02-27 22:40:23 +0000

    home/gnome: pin paperwm and vitals to GNOME 49 versions

--- modules/home-manager/desktop/gnome.nix
+++ modules/home-manager/desktop/gnome.nix
@@ -1,4 +1,31 @@
-{ pkgs-unstable, ... }: rec {
+{ pkgs, pkgs-unstable, ... }:
+let
+  paperwm = pkgs-unstable.gnomeExtensions.paperwm.overrideAttrs (_: {
+    version = "145";
+    src = pkgs.fetchzip {
+      name = "paperwm-145.zip";
+      url = "https://extensions.gnome.org/download-extension/paperwm@paperwm.github.com.shell-extension.zip?version_tag=68525";
+      hash = "sha256-V0bEHC557t0pGHf5aMMZhq16nmiXqr7DImOj5a3btS4=";
+      stripRoot = false;
+    };
+  });
+
+  vitals = pkgs-unstable.gnomeExtensions.vitals.overrideAttrs (_: {
+    version = "73";
+    src = pkgs.fetchzip {
+      name = "vitals-73.zip";
+      url = "https://extensions.gnome.org/download-extension/Vitals@CoreCoding.com.shell-extension.zip?version_tag=64741";
+      hash = "sha256-0cvFXTa2UUu44IqbFI1a13zGJuZ9ymfqdCZMAyGKgPc=";
+      stripRoot = false;
+    };
+  });
+
+  gnomeExtensions = [
+    paperwm
+    vitals
+  ];
+in
+{
   imports = [ ./dconf.nix ];
 
   # Disable GNOME SSH agent components (unreliable - randomly set wrong SSH_AUTH_SOCK)
@@ -12,12 +39,9 @@
     Hidden=true
   '';
 
-  home.packages = with pkgs-unstable.gnomeExtensions; [
-    paperwm
-    vitals
-  ];
+  home.packages = gnomeExtensions;
   dconf.settings = {
-    "org/gnome/shell".enabled-extensions = map (extension: extension.extensionUuid) home.packages;
+    "org/gnome/shell".enabled-extensions = map (extension: extension.extensionUuid) gnomeExtensions;
     "org/gnome/shell".disabled-extensions = [ ];
   };
 
