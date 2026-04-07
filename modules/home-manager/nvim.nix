6392f85c2c489e00b40c129359fa672b9fee4c9d
Oleg Martynov <fjolne.yngling@gmail.com>
2026-04-07 19:39:41 +0100

    neovim: rm molten

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -10,18 +10,11 @@ in
 
   # Neovim plugins from nix (symlinked to packpath)
   xdg.dataFile."nvim/site/pack/nix/start/gruvbox-nvim".source = pkgs.vimPlugins.gruvbox-nvim;
-  xdg.dataFile."nvim/site/pack/nix/start/molten-nvim".source = pkgs.vimPlugins.molten-nvim;
-  xdg.dataFile."nvim/site/pack/nix/start/image-nvim".source = pkgs.vimPlugins.image-nvim;
   xdg.dataFile."nvim/site/pack/nix/start/plenary-nvim".source = pkgs.vimPlugins.plenary-nvim;
   xdg.dataFile."nvim/site/pack/nix/start/telescope-nvim".source = pkgs.vimPlugins.telescope-nvim;
   xdg.dataFile."nvim/site/pack/nix/start/gitsigns-nvim".source = pkgs.vimPlugins.gitsigns-nvim;
-
   home.packages = with pkgs; [
-    # Neovim with magick (for image.nvim) and python packages (for molten-nvim)
-    (wrapNeovim neovim-unwrapped {
-      extraLuaPackages = luaPkgs: [ luaPkgs.magick ];
-      extraPython3Packages = ps: [ ps.pynvim ps.jupyter-client ps.cairosvg ps.pnglatex ps.plotly ps.pyperclip ];
-    })
+    neovim-unwrapped
 
     # LSP servers
     nil
@@ -29,8 +22,5 @@ in
     pyright
     rust-analyzer
     typescript-language-server
-
-    # Molten/image.nvim dependencies
-    imagemagick
   ];
 }
