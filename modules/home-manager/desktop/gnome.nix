dcb8fcd41b992def587ea30b26da205397d5d1f4
Oleg Martynov <fjolne.yngling@gmail.com>
2026-04-11 21:46:43 +0100

    update nixpkgs-unstable

--- modules/home-manager/desktop/gnome.nix
+++ modules/home-manager/desktop/gnome.nix
@@ -1,24 +1,26 @@
 { pkgs, pkgs-unstable, ... }:
 let
-  paperwm = pkgs-unstable.gnomeExtensions.paperwm.overrideAttrs (_: {
-    version = "145";
-    src = pkgs.fetchzip {
-      name = "paperwm-145.zip";
-      url = "https://extensions.gnome.org/download-extension/paperwm@paperwm.github.com.shell-extension.zip?version_tag=68525";
-      hash = "sha256-V0bEHC557t0pGHf5aMMZhq16nmiXqr7DImOj5a3btS4=";
-      stripRoot = false;
-    };
-  });
-
-  vitals = pkgs-unstable.gnomeExtensions.vitals.overrideAttrs (_: {
-    version = "73";
-    src = pkgs.fetchzip {
-      name = "vitals-73.zip";
-      url = "https://extensions.gnome.org/download-extension/Vitals@CoreCoding.com.shell-extension.zip?version_tag=64741";
-      hash = "sha256-0cvFXTa2UUu44IqbFI1a13zGJuZ9ymfqdCZMAyGKgPc=";
-      stripRoot = false;
-    };
-  });
+  paperwm = pkgs-unstable.gnomeExtensions.paperwm;
+  # .overrideAttrs (_: {
+  #   version = "145";
+  #   src = pkgs.fetchzip {
+  #     name = "paperwm-145.zip";
+  #     url = "https://extensions.gnome.org/download-extension/paperwm@paperwm.github.com.shell-extension.zip?version_tag=68525";
+  #     hash = "sha256-V0bEHC557t0pGHf5aMMZhq16nmiXqr7DImOj5a3btS4=";
+  #     stripRoot = false;
+  #   };
+  # });
+  #
+  vitals = pkgs-unstable.gnomeExtensions.vitals;
+  # .overrideAttrs (_: {
+  #   version = "73";
+  #   src = pkgs.fetchzip {
+  #     name = "vitals-73.zip";
+  #     url = "https://extensions.gnome.org/download-extension/Vitals@CoreCoding.com.shell-extension.zip?version_tag=64741";
+  #     hash = "sha256-0cvFXTa2UUu44IqbFI1a13zGJuZ9ymfqdCZMAyGKgPc=";
+  #     stripRoot = false;
+  #   };
+  # });
 
   gnomeExtensions = [
     paperwm
