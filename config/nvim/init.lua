3c8ec77da0b61ccd1a2d126a2d8bf4b056e0d6f8
Oleg Martynov <fjolne.yngling@gmail.com>
2026-05-06 09:45:57 +0100

    nvim: C-h for search all files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -248,6 +248,7 @@ if telescope_ok then
     })
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<C-h>", function() builtin.find_files({ hidden = true, no_ignore = true }) end, { desc = "Find files including hidden and ignored" })
     vim.keymap.set("n", "<leader>ff", builtin.live_grep, { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
